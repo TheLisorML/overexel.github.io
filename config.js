b269f8cf7eeb6c2f1e828a023f466d590cbb1d79
TheLisorML <92590762+TheLisorML@users.noreply.github.com>
2022-05-28 17:05:20 +0300

    Add files via upload

---
+++ config.js
@@ -0,0 +1,9 @@
+var server_name = "Overexel";
+var server_ip = "de1.qloxhost.xyz";
+var server_port = 25444;
+var ip_copied = "IP copied!";
+
+var firefly_count = 20;
+
+staff("TheLisorML", "", "Owner, developer");
+staff("", "", "Owner");
